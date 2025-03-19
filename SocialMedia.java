b0f44a8051b94df75fabc3e40b98ef5eed15a069
ankush1038 <103501353+ankush1038@users.noreply.github.com>
2025-03-19 09:58:20 +0530

    Update SocialMedia.java

--- SocialMedia.java
+++ SocialMedia.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SocialMedia {
@@ -20,7 +19,7 @@ public class SocialMedia {
             System.out.print("Enter your choice: ");
 
             int choice = sc.nextInt();
-            sc.nextLine(); // Consume newline
+            sc.nextLine();
 
             switch (choice) {
                 case 1:
@@ -92,35 +91,40 @@ public class SocialMedia {
     }
 }
 
-// Class representing a User in the social network
-class User {
-    int userId;
-    String name;
-    int age;
-    ArrayList<Integer> friends;
-    User next;
+class FriendNode {
+    int friendId;
+    FriendNode next;
 
-    // Constructor to initialize a user
-    public User(int userId, String name, int age) {
-        this.userId = userId;
-        this.name = name;
-        this.age = age;
-        this.friends = new ArrayList<>();
+    public FriendNode(int friendId) {
+        this.friendId = friendId;
         this.next = null;
     }
 }
 
-// Class managing the friend network using a singly linked list
+class User {
+    int userId;
+    String name;
+    int age;
+    FriendNode friends;
+    User next;
+
+    public User(int userId, String name, int age) {
+        this.userId = userId;
+        this.name = name;
+        this.age = age;
+        this.friends = null;
+        this.next = null;
+    }
+}
+
 class FriendNetwork {
     private User head;
 
-    // Method to add a new user
     public void addUser(int userId, String name, int age) {
         if (getUserById(userId) != null) {
             System.out.println("User ID already exists!");
             return;
         }
-
         User newUser = new User(userId, name, age);
         if (head == null) {
             head = newUser;
@@ -134,7 +138,6 @@ class FriendNetwork {
         System.out.println("User added successfully.");
     }
 
-    // Method to get a user by their ID
     private User getUserById(int userId) {
         User temp = head;
         while (temp != null) {
@@ -146,7 +149,6 @@ class FriendNetwork {
         return null;
     }
 
-    // Method to add a friend connection
     public void addFriend(int userId1, int userId2) {
         User user1 = getUserById(userId1);
         User user2 = getUserById(userId2);
@@ -156,112 +158,44 @@ class FriendNetwork {
             return;
         }
 
-        if (!user1.friends.contains(userId2)) {
-            user1.friends.add(userId2);
-            user2.friends.add(userId1);
+        if (!isFriend(user1.friends, userId2)) {
+            user1.friends = new FriendNode(userId2, user1.friends);
+            user2.friends = new FriendNode(userId1, user2.friends);
             System.out.println("Friend connection added.");
         } else {
             System.out.println("Users are already friends.");
         }
     }
 
-    // Method to remove a friend connection
-    public void removeFriend(int userId1, int userId2) {
-        User user1 = getUserById(userId1);
-        User user2 = getUserById(userId2);
-
-        if (user1 == null || user2 == null) {
-            System.out.println("One or both users not found.");
-            return;
-        }
-
-        if (user1.friends.contains(userId2)) {
-            user1.friends.remove(Integer.valueOf(userId2));
-            user2.friends.remove(Integer.valueOf(userId1));
-            System.out.println("Friend connection removed.");
-        } else {
-            System.out.println("Users are not friends.");
-        }
-    }
-
-    // Method to find mutual friends between two users
-    public void findMutualFriends(int userId1, int userId2) {
-        User user1 = getUserById(userId1);
-        User user2 = getUserById(userId2);
-
-        if (user1 == null || user2 == null) {
-            System.out.println("One or both users not found.");
-            return;
-        }
-
-        System.out.print("Mutual Friends: ");
-        boolean found = false;
-        for (int friendId : user1.friends) {
-            if (user2.friends.contains(friendId)) {
-                System.out.print(friendId + " ");
-                found = true;
+    private boolean isFriend(FriendNode head, int friendId) {
+        FriendNode temp = head;
+        while (temp != null) {
+            if (temp.friendId == friendId) {
+                return true;
             }
+            temp = temp.next;
         }
-
-        if (!found) {
-            System.out.println("No mutual friends found.");
-        } else {
-            System.out.println();
-        }
+        return false;
     }
 
-    // Method to display all friends of a user
     public void displayFriends(int userId) {
         User user = getUserById(userId);
-
         if (user == null) {
             System.out.println("User not found.");
             return;
         }
 
-        if (user.friends.isEmpty()) {
+        FriendNode temp = user.friends;
+        if (temp == null) {
             System.out.println(user.name + " has no friends.");
             return;
         }
 
         System.out.print(user.name + "'s Friends: ");
-        for (int friendId : user.friends) {
-            System.out.print(friendId + " ");
+        while (temp != null) {
+            System.out.print(temp.friendId + " ");
+            temp = temp.next;
         }
         System.out.println();
-    }
-
-    // Method to search a user by their ID
-    public void searchUserById(int userId) {
-        User user = getUserById(userId);
-
-        if (user == null) {
-            System.out.println("User not found.");
-            return;
-        }
-
-        System.out.println("User Found: ID: " + user.userId + ", Name: " + user.name + ", Age: " + user.age);
-    }
-
-    // Method to search a user by their name
-    public void searchUserByName(String name) {
-        User temp = head;
-        while (temp != null) {
-            if (temp.name.equalsIgnoreCase(name)) {
-                System.out.println("User Found: ID: " + temp.userId + ", Name: " + temp.name + ", Age: " + temp.age);
-                return;
-            }
-            temp = temp.next;
-        }
-        System.out.println("User not found.");
-    }
-
-    // Method to count the number of friends for each user
-    public void countFriends() {
-        User temp = head;
-        while (temp != null) {
-            System.out.println(temp.name + " has " + temp.friends.size() + " friends.");
-            temp = temp.next;
-        }
     }
 }
